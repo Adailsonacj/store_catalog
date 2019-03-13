92929443cc571f0924f336d3b79482a38e64d9a9
Adailsonacj <adailsonacj@live.com>
2019-03-13 11:17:18 -0300

    Apresentando o ciclo de vida dos componentes na classe Main/arquivo index.js

--- index.js
+++ index.js
@@ -2,8 +2,32 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import React, { Component } from 'react'
+import { AppRegistry } from 'react-native';
+import ListaItens from './src/components/ListaItens'
 
-AppRegistry.registerComponent(appName, () => App);
+class Main extends Component {
+    constructor(props) {
+        super(props)
+        console.log('construindo a aplicação')
+    }
+
+    componentWillMount() {
+        console.log('Fazendo algo antes de renderizar')
+
+    }
+
+    componentDidMount() {
+        console.log('Fazendo algo depois de renderizar')
+
+    }
+
+    render() {
+        console.log('Componente renderizado')
+        return (
+            <ListaItens />
+        )
+    }
+}
+
+AppRegistry.registerComponent('store_catalog', () => Main);
